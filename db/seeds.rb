b3bbbfbc55ff58da95a2d6ab2f15e4dd53627884
Grant Davis <grantdavis303@gmail.com>
2024-07-08 12:51:58 -0600

    Create Seeds File

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,41 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+grant = User.create!(first_name: "Grant", last_name: "Davis", email: "grantdavis303@gmail.com", password_digest: "dummy123")
+
+habit_1 = Habit.create!(name: "Brush Teeth", category: "Dental")
+habit_2 = Habit.create!(name: "Floss Teeth", category: "Dental")
+habit_3 = Habit.create!(name: "Run", category: "Exercise")
+
+UserHabit.create!(
+  user_id: grant.id,
+  habit_id: habit_1.id,
+  status: 'active',
+  frequency_int: 2,
+  frequency_type: 'day',
+  started_date: '7/8/2024',
+  times_completed: 0,
+  days_completed: 0,
+  weeks_completed: 0
+)
+
+UserHabit.create!(
+  user_id: grant.id,
+  habit_id: habit_2.id,
+  status: 'active',
+  frequency_int: 2,
+  frequency_type: 'day',
+  started_date: '7/8/2024',
+  times_completed: 0,
+  days_completed: 0,
+  weeks_completed: 0
+)
+
+UserHabit.create!(
+  user_id: grant.id,
+  habit_id: habit_2.id,
+  status: 'active',
+  frequency_int: 3,
+  frequency_type: 'week',
+  started_date: '7/8/2024',
+  times_completed: 0,
+  days_completed: 0,
+  weeks_completed: 0
+)
