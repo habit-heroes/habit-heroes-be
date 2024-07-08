627d2f9c9c42eb765ec55cec88a8013adb547124
Grant Davis <grantdavis303@gmail.com>
2024-07-08 12:52:43 -0600

    Create Migrations Ran File

--- db/migrations_ran.rb
+++ db/migrations_ran.rb
@@ -1,5 +1,8 @@
 # Migrations Ran
 
 # List migrations here
+rails generate model User first_name last_name email password_digest
+rails generate model Habit name category
+rails generate model UserHabit user:references habit:references status frequency_int:integer frequency_type started_date times_completed:integer days_completed:integer weeks_completed:integer
 
 rails db:migrate
