3c94ce090ff80be55e331987403be8a2f7fc8f2c
Grant Davis <grantdavis303@gmail.com>
2024-07-08 12:36:48 -0600

    Create Users Show Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+      get "/users/:id", to: "users#show"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+    end
+  end
 end
