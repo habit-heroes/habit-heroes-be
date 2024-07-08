01ddff1d856cee8dc7a3683d86e8c8e245558088
Grant Davis <grantdavis303@gmail.com>
2024-07-08 12:53:47 -0600

    Update UserSerializer w/ JSONAPI Serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::UsersController < ApplicationController
   def show
     user = User.find_by(id: params[:id])
     if !user.nil?
-      render json: UserSerializer.new.serialized_json(user)
+      render json: UserSerializer.new(user), status: 200
     else
-      render json: { error: "User not found" }
+      render json: { error: "User not found" }, status: 404
     end
   end
 end
