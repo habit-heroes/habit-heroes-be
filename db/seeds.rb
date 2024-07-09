9b668344231d7ec06468ce6d3a189a9570fce457
Grant Davis <grantdavis303@gmail.com>
2024-07-08 21:09:08 -0600

    Add Habits to Seeds File

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,24 @@
 grant = User.create!(first_name: "Grant", last_name: "Davis", email: "grantdavis303@gmail.com", password_digest: "dummy123")
 brandon = User.create!(first_name: "Brandon", last_name: "Doza", email: "brandon123@gmail.com", password_digest: "password456")
 
-habit_1 = Habit.create!(name: "Brush Teeth", category: "Dental")
-habit_2 = Habit.create!(name: "Floss Teeth", category: "Dental")
-habit_3 = Habit.create!(name: "Run", category: "Exercise")
+habit_1 = Habit.create!(name: "Brush Teeth", category: 0)
+habit_2 = Habit.create!(name: "Floss Teeth", category: 0)
+habit_3 = Habit.create!(name: "Rinse w/ Mouth Wash", category: 0)
+habit_4 = Habit.create!(name: "Sleep 8 Hours", category: 1)
+habit_5 = Habit.create!(name: "Wake Up at 7am", category: 2)
+habit_6 = Habit.create!(name: "Drink 100oz of Water", category: 3)
+habit_7 = Habit.create!(name: "Read", category: 4)
+habit_8 = Habit.create!(name: "Write", category: 4)
+habit_9 = Habit.create!(name: "Walk 10,000 Steps", category: 5)
+habit_10 = Habit.create!(name: "Lift Weights", category: 5)
 
 UserHabit.create!(
   user_id: grant.id,
   habit_id: habit_1.id,
-  status: 'active',
-  frequency_int: 2,
-  frequency_type: 'day',
-  started_date: '7/8/2024',
+  status: 1,
+  goal_int: 2,
+  goal_type: 0,
+  started_date: Time.now,
   times_completed: 0,
   days_completed: 0,
   weeks_completed: 0
@@ -20,10 +27,10 @@ UserHabit.create!(
 UserHabit.create!(
   user_id: grant.id,
   habit_id: habit_2.id,
-  status: 'active',
-  frequency_int: 2,
-  frequency_type: 'day',
-  started_date: '7/8/2024',
+  status: 1,
+  goal_int: 2,
+  goal_type: 0,
+  started_date: Time.now,
   times_completed: 0,
   days_completed: 0,
   weeks_completed: 0
@@ -31,11 +38,11 @@ UserHabit.create!(
 
 UserHabit.create!(
   user_id: grant.id,
-  habit_id: habit_2.id,
-  status: 'active',
-  frequency_int: 3,
-  frequency_type: 'week',
-  started_date: '7/8/2024',
+  habit_id: habit_10.id,
+  status: 1,
+  goal_int: 3,
+  goal_type: 1,
+  started_date: Time.now,
   times_completed: 0,
   days_completed: 0,
   weeks_completed: 0
