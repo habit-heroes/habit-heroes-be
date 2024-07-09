0301ec3fb1463c4bf13a791997df98c9452bbb7a
Grant Davis <grantdavis303@gmail.com>
2024-07-09 06:49:07 -0600

    Create Route for Habits Index

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
-      get "/users/:id", to: "users#show"
-      get "/users/:id/habits", to: "user_habits#index"
+      get '/users/:id', to: 'users#show'
+      get '/users/:id/habits', to: 'user_habits#index'
+      get '/habits', to: 'habits#index'
 
     end
   end
