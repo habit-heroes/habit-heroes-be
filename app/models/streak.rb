967cf2fbcb9c547981f5894b7d1eb9e4f8387b07
Grant Davis <grantdavis303@gmail.com>
2024-07-10 13:03:29 -0600

    Update Streak and User Model

--- app/models/streak.rb
+++ app/models/streak.rb
@@ -1,8 +1,9 @@
 class Streak < ApplicationRecord
   belongs_to :user_habit
+  has_one :user, through: :user_habit
 
-  validates_presence_of :user_habit_id, :type
-  validates_numericality_of :user_habit_id, :type
+  validates_presence_of :user_habit_id, :streak_type
+  validates_numericality_of :user_habit_id, :streak_type
 
-  enum type: [:light, :fire]
+  enum streak_type: [:light, :fire]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :user_habits
   has_many :habits, through: :user_habits
+  has_many :streaks, through: :user_habits
 
   validates_presence_of :first_name, :last_name, :email, :password_digest
   validates_uniqueness_of :email
