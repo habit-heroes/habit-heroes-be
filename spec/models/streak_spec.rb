4bcfce6880b2d3e23dc7d429de959c5783a6d47b
Grant Davis <grantdavis303@gmail.com>
2024-07-10 12:58:44 -0600

    Update Streak Model Tests

--- spec/models/streak_spec.rb
+++ spec/models/streak_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe Streak, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:user_habit_id) }
-    it { should validate_presence_of(:type) }
+    it { should validate_presence_of(:streak_type) }
     it { should validate_numericality_of(:user_habit_id) }
   end
 
   describe 'enums' do
-    it { should define_enum_for(:type).with_values([:light, :fire]) }
+    it { should define_enum_for(:streak_type).with_values([:light, :fire]) }
   end
 end
