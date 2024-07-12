46293eede4a470657be924a2dc7ced861a5698d2
Grant Davis <grantdavis303@gmail.com>
2024-07-12 16:52:06 -0600

    Create Habit Helper Methods + Tests

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -5,4 +5,22 @@ class Habit < ApplicationRecord
   validates_presence_of :name, :category
 
   enum category: [:dental, :sleep, :productivity, :hydration, :hobby, :exercise]
+
+  def week_type
+    if category == "hobby" || category == "exercise"
+      return 1
+    else
+      return 0
+    end
+  end
+
+  def determine_frequency
+    if week_type == 0 && category == "dental"
+      return 2
+    elsif week_type == 0
+      return 1
+    else
+      return 3
+    end
+  end
 end
